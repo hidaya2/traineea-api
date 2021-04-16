f436e840b30957a3fd6a7b5727dd6d8d4560ec55
hidaya2 <kandahkunateh@gmail.com>
2021-04-16 09:44:09 -0700

    added file

--- routes/traineesRoute.js
+++ routes/traineesRoute.js
@@ -1,9 +1,8 @@
 const express = require("express")
 const { createTrainee, getAllTrainees,getSingleTrainee,updateTrainee, deleteTrainee } = require("../controllers/traineesController")
-const protect = require("../middlewares/authMiddleware")
 const router = express.Router()
 
-router.route("/").post(protect, createTrainee).get(protect, getAllTrainees)
-router.route("/:_id").get(protect, getSingleTrainee).put(protect, updateTrainee).delete(protect,deleteTrainee)
+router.route("/").post(createTrainee).get(getAllTrainees)
+router.route("/:_id").get(getSingleTrainee).put(updateTrainee).delete(deleteTrainee)
 
 module.exports = router
